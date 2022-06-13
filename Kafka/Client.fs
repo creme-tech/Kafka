a523fb4f97646067478e7c4626068416831a863e
Weslen Guerreiro <savi2w@icloud.com>
2022-06-13 09:02:14 -0300

    feat: Initial commit

---
+++ Kafka/Client.fs
@@ -0,0 +1,29 @@
+﻿module KafkaClient
+
+open Confluent.Kafka
+open System
+open System.Collections.Generic
+
+let private getConfig () =
+    let config = Dictionary<string, string>()
+
+    config.Add("bootstrap.servers", Environment.GetEnvironmentVariable "AK_SERVER")
+    config.Add("security.protocol", "SASL_SSL")
+    config.Add("sasl.mechanisms", "PLAIN")
+    config.Add("sasl.username", Environment.GetEnvironmentVariable "AK_USERNAME")
+    config.Add("sasl.password", Environment.GetEnvironmentVariable "AK_PASSWORD")
+    config.Add("session.timeout.ms", "45000")
+
+    config
+
+let getAdminClientBuilder () =
+    let config = getConfig ()
+    let adminClientBuilder = AdminClientBuilder config
+
+    adminClientBuilder
+
+let getKafkaClientConfig () =
+    let config = getConfig ()
+    let kafkaClientConfig = ClientConfig config
+
+    kafkaClientConfig
---
+++ README.md
@@ -0,0 +1,12 @@
+# Kafka
+
+A simple example of running [Apache Kafka](https://kafka.apache.org/) with F# 
+
+## Run
+
+1. Set `AK_SERVER`, `AK_USERNAME` and `AK_PASSWORD` environment variables
+2. Build and run as usual
+
+## License
+
+This project is distributed under the [Apache License 2.0](LICENSE)
