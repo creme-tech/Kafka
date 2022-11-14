85bba27f78831b273f932019c15e76184df77070
Weslen Guerreiro <omppye@gmail.com>
2022-11-14 12:05:12 -0300

    chore: Change Fantomas mode to G-Research

--- Kafka/Program.fs
+++ Kafka/Program.fs
@@ -12,20 +12,21 @@ let main _ =
     |> Async.AwaitTask
     |> Async.RunSynchronously
 
-    let message = Message<string, string>()
+    let message = Message<string, string> ()
 
     message.Key <- "message-key"
     message.Value <- "message-value"
 
     let producer =
-        new Thread(
-            new ThreadStart(fun () ->
+        new Thread (
+            new ThreadStart (fun () ->
                 Thread.Sleep 2048 (* Delay to consumer set up properly *)
 
                 Producer.produceMessage clientConfig topicName message
-                |> ignore)
+                |> ignore
+            )
         )
 
-    producer.Start()
+    producer.Start ()
 
     Consumer.consumeTopic clientConfig topicName
